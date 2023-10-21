19506ecb57908235670fc6c194e176ea5807245f
yazeed505 <yazeedalqahtani123@gmail.com>
2023-10-21 08:42:17 +0300

    Update Docker image tag in Helm chart

--- airports/airports.sh
+++ airports/airports.sh
@@ -2,7 +2,7 @@
 
 # Set your Docker image name and version
 DOCKER_IMAGE_NAME="airports-assembly"
-DOCKER_IMAGE_VERSION="1.1.0"
+DOCKER_IMAGE_VERSION="1.0.1"
 
 # Set your Helm chart directory
 HELM_CHART_DIR="./helm"
